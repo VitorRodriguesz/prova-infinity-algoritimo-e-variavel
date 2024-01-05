3ce3e8b44275d9b63e1644a01b783ff7b9166bbd
vitor rodrigues <143138568+VitorRodriguesz@users.noreply.github.com>
2024-01-04 21:04:41 -0300

    Add files via upload

---
+++ provainfinity1.py
@@ -0,0 +1,15 @@
+(int) idade = 25
+
+(float)altura = 1.75
+
+(str)nome = "João"
+
+(bool)tem_carro = True
+
+entrada_usuario = input("Digite um número: ")
+
+o (nesse caso, inteiro) numero_digitado = int(entrada_usuario)
+
+print(f"Você digitou o número: {numero_digitado}")
+
+print(f"Nome: {nome}, Idade: {idade}, Altura: {altura}, Tem Carro: {tem_carro}")
